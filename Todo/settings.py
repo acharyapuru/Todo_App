a3a1ffdc6f1d6392333d3fe385436d71b64c628a
acharyapuru <acharyapuru05@gmail.com>
2023-07-01 21:39:13 +0545

    v 1

--- Todo/settings.py
+++ Todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import decouple
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5=n4xn%58m1*14ulosr7ar6@i8psg!7g47+4^q1q80)zu$6ez$'
+SECRET_KEY = decouple.config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = decouple.config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
